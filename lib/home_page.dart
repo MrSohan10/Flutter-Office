240203351ea6720d5a2639e4d0a2343295a8cbac
MrSohan10 <iamsohan11@gmail.ocm>
2023-09-28 13:15:15 +0600

    Practice Fractionally SizeBox

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,17 +3,14 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('AppBar'),
-        titleSpacing: 10,
-        elevation: 20,
-        toolbarHeight: 60,
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.message)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.person))
-        ],
-      ),
+      appBar: AppBar(title: Text('AppBar')),
+      body: Center(
+          child: LinearProgressIndicator(
+        value: 50,
+        // backgroundColor: Colors.grey,
+        color: Colors.blue,
+        minHeight: 10,
+      )),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,51 +7,19 @@ void main() {
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('GridView'),
-            ),
-            body: GridView(
-              padding: EdgeInsets.all(5),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, crossAxisSpacing: 2, mainAxisSpacing: 2),
-              children: [
-                Container(
-                  height: 200,
-                  width: 200,
-                  color: Colors.grey,
-                ),
-                Container(
-                  height: 200,
-                  width: 200,
-                  color: Colors.grey,
-                ),
-                Container(
-                  height: 200,
-                  width: 200,
-                  color: Colors.grey,
-                ),
-                Container(
-                  height: 200,
-                  width: 200,
-                  color: Colors.grey,
-                ),
-                Container(
-                  height: 200,
-                  width: 200,
-                  color: Colors.grey,
-                ),
-                Container(
-                  height: 200,
-                  width: 200,
-                  color: Colors.grey,
-                ),
-                Container(
-                  height: 200,
-                  width: 200,
-                  color: Colors.grey,
-                ),
-              ],
-            )));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Fractionally SizeBox'),
+        ),
+        body: Center(
+            child: FractionallySizedBox(
+          widthFactor: 0.5,
+          heightFactor: 0.5,
+          child: Container(
+            color: Colors.blue,
+          ),
+        )),
+      ),
+    );
   }
 }
